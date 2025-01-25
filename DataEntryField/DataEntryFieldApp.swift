71ab6d012b25f15f6a3726e4e4afb4fafd9011c1
Knexxxx <knexxxx@gmail.com>
2025-01-25 18:50:45 +0100

    Initial Commit

---
+++ DataEntryField/DataEntryFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataEntryFieldApp.swift
+//  DataEntryField
+//
+//  Created by Andreas on 25.01.25.
+//
+
+import SwiftUI
+
+@main
+struct DataEntryFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
